2acad8485d8e4b08d3300affc143fd1893bc990f
menge-mm <mfn@modern-miracle.com>
2024-10-09 16:01:40 +0300

    next-js documentation

---
+++ pages/_meta.js
@@ -0,0 +1,15 @@
+export default {
+  index: 'Welcome',
+  introduction: 'Introduction',
+  'getting-started': 'Getting Started',
+  contracts: 'Contracts',
+  data_types: 'Data Types',
+  interfaces: 'Interfaces',
+  'api-docs': 'API Documentation',
+  encrypt: 'Encryption',
+  github_link: {
+    title: 'Source Code ↗',
+    href: 'https://github.com/shuding/nextra',
+    newWindow: true,
+  },
+};
---
+++ pages/contracts/_meta.js
@@ -0,0 +1,7 @@
+export default {
+  consent: 'Consent Management',
+  data_registry: 'Data Registry',
+  compensation: 'Compensation',
+  'ledup-token': 'LED-UP Token',
+  zkp: 'ZKP',
+};
